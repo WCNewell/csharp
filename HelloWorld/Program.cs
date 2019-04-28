f95c6f2eed61c227402c5ae8663faf72772be6fe
W. Clark Newell <clark@clarknewell.tech>
2019-04-27 21:32:51 -0600

    initial commit

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HelloWorld
 {
@@ -10,6 +6,28 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            //byte number = 2;
+            //int count = 10;
+            //float totalPrice = 20.95f;
+            //char character = 'A';
+            //string firstName = "Clark";
+            //bool isWorking = false;
+
+            // var can also be used and c# compiler will detect the data type
+
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'A';
+            string firstName = "Clark";
+            bool isWorking = false;
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
         }
     }
 }
