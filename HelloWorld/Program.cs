1244cde557f84ffd2075c8acdcd013df3b1089db
W. Clark Newell <clark@clarknewell.tech>
2019-05-05 20:19:37 -0600

    working with random class

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -23,20 +23,91 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            var season = Season.Autumn;
+            var random = new Random();
 
-            switch (season)
-            {
-                case Season.Autumn:
-                case Season.Summer:
-                    Console.WriteLine("Some like it sunny");
-                    break;
+            const int passwordLength = 13;
 
-                default:
-                    Console.WriteLine("The other two seasons are very wet");
-                    break;
-            }
+            var buffer = new char[passwordLength];
+            for (var i = 0; i < passwordLength; i++)
+                buffer[i] = (char)('a' + random.Next(0, 26));
 
+            var password = new string(buffer);
+
+            Console.WriteLine(password);
+
+            //while (true)
+            //{
+            //    Console.Write("Type your name");
+            //    var input = Console.ReadLine();
+
+            //    if (!String.IsNullOrWhiteSpace(input))
+            //    {
+            //        Console.WriteLine("@Echo: " + input);
+            //        continue;
+            //    }
+
+            //    break;
+            //}
+
+            //var i = 0;
+            //while (i <= 10)
+            //{
+            //    if (i % 2 == 0)
+            //        Console.WriteLine(i);
+
+            //    i++;
+            //}
+
+            //var numbers = new int[] {1, 2, 3, 4};
+
+            //foreach (var number in numbers)
+            //{
+            //    Console.WriteLine(number);
+            //}
+
+            //var name = "Clark Newell";
+
+            //for (var i = 0; i < name.Length; i++)
+            //{
+            //    Console.WriteLine(name[i]);
+            //}
+
+            //foreach (var character in name)
+            //{
+            //    Console.WriteLine(character);
+            //}
+
+            //for (var i = 1; i <= 10; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+
+            //}
+
+            //for (var i = 10; i >= 1; i--)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+
+            //}
+
+            //var season = Season.Autumn;
+
+            //switch (season)
+            //{
+            //    case Season.Autumn:
+            //    case Season.Summer:
+            //        Console.WriteLine("Some like it sunny");
+            //        break;
+
+            //    default:
+            //        Console.WriteLine("The other two seasons are very wet");
+            //        break;
+            //}
 
             //bool isGoldCustomer = true;
 
