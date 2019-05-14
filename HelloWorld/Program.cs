70a3af46ffb7b461af3c89cd53fcf8a46dbcb18d
W. Clark Newell <clark@clarknewell.tech>
2019-05-13 21:23:04 -0600

    working with strings

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Xml;
@@ -25,29 +26,46 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            var timeSpan = new TimeSpan(1, 2, 3);
-            var timeSpan1 = new TimeSpan(1, 0, 0);
+            var fullName = "William Newell ";
+            Console.WriteLine("Trim: '{0}'", fullName.Trim());
+            Console.WriteLine("ToUpper: '{0}'", fullName.Trim().ToUpper());
 
-            var timeSpan2 = TimeSpan.FromHours(1);
+            var index = fullName.IndexOf(' ');
+            var firstName = fullName.Substring(0, index);
+            var lastName = fullName.Substring(index + 1);
+            Console.WriteLine("First Name: " + firstName);
+            Console.WriteLine("Last Name: " + lastName);
 
-            var start = DateTime.Now;
-            var end = DateTime.Now.AddMinutes(2);
-            var duration = end - start;
-            Console.WriteLine("Duration: " + duration);
+            var names = fullName.Split(' ');
+            Console.WriteLine("First Name: " + names[0]);
+            Console.WriteLine("Last Name: " + names[1]);
 
-            // Properties
-            Console.WriteLine("Minutes: " + timeSpan.Minutes);
-            Console.WriteLine("Total Minutes: " + timeSpan.TotalMinutes);
+            fullName.Replace("William", "Clark");
 
-            // Add
-            Console.WriteLine("Add Example: " + timeSpan.Add(TimeSpan.FromMinutes(8)));
-            Console.WriteLine("Subtract Example: " + timeSpan.Subtract(TimeSpan.FromMinutes(2)));
 
-            // To String
-            Console.WriteLine("ToString: " + timeSpan.ToString());
+            //var timeSpan = new TimeSpan(1, 2, 3);
+            //var timeSpan1 = new TimeSpan(1, 0, 0);
 
-            // Parse
-            Console.WriteLine("Parse: " + TimeSpan.Parse("01:02:03"));
+            //var timeSpan2 = TimeSpan.FromHours(1);
+
+            //var start = DateTime.Now;
+            //var end = DateTime.Now.AddMinutes(2);
+            //var duration = end - start;
+            //Console.WriteLine("Duration: " + duration);
+
+            //// Properties
+            //Console.WriteLine("Minutes: " + timeSpan.Minutes);
+            //Console.WriteLine("Total Minutes: " + timeSpan.TotalMinutes);
+
+            //// Add
+            //Console.WriteLine("Add Example: " + timeSpan.Add(TimeSpan.FromMinutes(8)));
+            //Console.WriteLine("Subtract Example: " + timeSpan.Subtract(TimeSpan.FromMinutes(2)));
+
+            //// To String
+            //Console.WriteLine("ToString: " + timeSpan.ToString());
+
+            //// Parse
+            //Console.WriteLine("Parse: " + TimeSpan.Parse("01:02:03"));
 
 
             //var dateTime = new DateTime(2015, 1, 1);
